1a53104daf31fc8f494478adc685f82d43136966
Milan9135 <Milpower0@gmail.com>
2024-04-26 15:59:43 +0200

    basic version

---
+++ website/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Void</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Welcome to my website :)</h1>
+    
+    <!-- Code box -->
+    <section class="box" id="code">
+
+    <h2>Code</h2>
+    <p>
+        <a href="">Birthdate tracker (under construction)</a>
+        <p>Add birthdays and see who's birthday is soon or search up specific people</p>
+    </p>
+    <p>
+        <a href="">Text duplicator (under construction)</a>
+        <p>Duplicate text for whatever reason</p>
+    </p>
+
+    </section>
+    <!-- End code box -->
+
+    <!-- About me box -->
+    <section class="box" id="about-me">
+
+    <h2>About me</h2>
+    <h4>Milan</h4>
+    <p>
+        Im a student studying software development for 2 years now.
+        I like to work on projects, program games and make websites,
+        this website is made to display all of these things in one place
+    </p>
+
+    </section>
+    <!-- End about me box -->
+
+    <!-- Games box -->
+    <section class="box" id="games">
+
+    <h2>Games</h2>
+    <p>
+        <a href="">Stack master (under construction)</a>
+        <p>A simple game about stacking blocks to make a tower as high as you can</p>
+    </p>
+    <!-- make display of versions and what they add -->
+
+    </section>
+    <!-- End games box -->
+
+    <h3>styling is to be added later :p</h3>
+</body>
+</html>
